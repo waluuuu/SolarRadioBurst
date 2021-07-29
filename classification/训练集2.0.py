87374c07ad04c45269850ef1e751e5b2f6714a4b
HFY <hfy0325@163.com>
2021-07-29 19:59:02 +0800

    更换训练集，新的二分类结果，尝试进行三分类

--- classification/训练集2.0.py
+++ classification/训练集2.0.py
@@ -5,9 +5,10 @@
 # @Software : PyCharm
 
 # 这是一种新的思路，所以用2来开头
+# 2.1训练集，每次使用之前，最好检查一下源码，是否有需要修改的地方
 
 from Classes.ImageChoice import ImageChoice
 
 read = r'G:\useful_L\reshape'
-write = r'G:\useful_L\NEW_NET_2.0'
+write = r'G:\useful_L\NEW_NET_2.1'
 IC = ImageChoice(read, write).action()
--- classification/训练集3.0.py
+++ classification/训练集3.0.py
@@ -1,26 +1,27 @@
 # -*- coding = utf-8 -*-
-# @Time : 2021/7/29 9:35
+# @Time : 2021/7/29 16:53
 # @Author : 水神与月神
-# @File : ImageChoice.py
+# @File : 训练集3.0.py
 # @Software : PyCharm
 
+# 这次从二分类中加了III型爆，变成了三分类，用3.0来命名
+# 由于有较大偏差，需要重写ImageChoice类的方法
 
 import os
 import shutil
 import random
+from Classes.ImageChoice import ImageChoice
 
 
-class ImageChoice:
+class ImageChoice2(ImageChoice):
     def __init__(self, path_read, path_write):
         # 读取路径
-        self._path_read = path_read
-        # 保存路径，保存训练集和验证集
-        self._path_write = path_write
+        super().__init__(path_read, path_write)
         # 每种类型，训练集和验证集数目
-        self._num = {'2L': [240, 100], '3': [500, 210], '4': [500, 210], 'count': [500, 210],
-                     'calibration': [250, 110], 'mixnoburst': [1490, 620]}
+        self._num = {'2L': [200, 60], '3': [1000, 300], '4': [400, 120], 'count': [400, 120],
+                     'calibration': [250, 110], 'noburst': [750, 190]}
         # 每种类型的二级目录
-        self._sub_directory = ['2L 340', '3 1387', '4 1205', 'calibration 363', 'count 1881', 'mixnoburst']
+        self._sub_directory = ['2L 340', '3 1387', '4 1205', 'calibration 363', 'count 1881', 'noburst 1998']
 
     def action(self):
         for sub_dir in self._sub_directory:
@@ -36,11 +37,13 @@ class ImageChoice:
             # 制作训练集
             for i in ran[0: self._num[sub_dir.split(' ')[0]][0]]:
                 filename = filenames[i]
-                # 判断是不是爆发，分别存储到不同的文件夹中
-                if sub_dir != 'calibration 363' and sub_dir != 'mixnoburst':
-                    sub_dir_write = 'train/burst'
-                else:
+                if sub_dir == 'calibration 363' or sub_dir == 'noburst 1998':
                     sub_dir_write = 'train/noburst'
+                elif sub_dir == '3 1387':
+                    sub_dir_write = 'train/III'
+                else:
+                    sub_dir_write = 'train/other'
+
                 # 将图片复制到新的文件夹
                 shutil.copy(os.path.join(self._path_read, sub_dir, filename),
                             os.path.join(self._path_write, sub_dir_write, filename))
@@ -49,12 +52,21 @@ class ImageChoice:
             # 制作验证集
             for i in ran[self._num[sub_dir.split(' ')[0]][0]:]:
                 filename = filenames[i]
-                # 判断是不是爆发
-                if sub_dir != 'calibration 363' and sub_dir != 'mixnoburst':
-                    sub_dir_write = 'val/burst'
-                else:
+                if sub_dir == 'calibration 363' or sub_dir == 'noburst 1998':
                     sub_dir_write = 'val/noburst'
+                elif sub_dir == '3 1387':
+                    sub_dir_write = 'val/III'
+                else:
+                    sub_dir_write = 'val/other'
                 # 将图片复制到新的文件夹
                 shutil.copy(os.path.join(self._path_read, sub_dir, filename),
                             os.path.join(self._path_write, sub_dir_write, filename))
                 print('图片{:s}已保存'.format(filename))
+
+
+if __name__ == '__main__':
+    read = r'G:\useful_L\reshape'
+    write = r'G:\useful_L\NEW_NET_3.0'
+    ll = ImageChoice2(read, write).action()
+
+
